0ea64d778687c8f268fd1395280b9d9660da1231
lijianqiang <newphper@gmail.com>
2018-09-02 15:13:49 +0800

    2.7.1

--- src/test/java/com/bytehonor/standard/api/protocol/util/MD5UtilsTest.java
+++ src/test/java/com/bytehonor/standard/api/protocol/util/MD5UtilsTest.java
@@ -8,7 +8,11 @@ public class MD5UtilsTest {
 
 	@Test
 	public void testMd5() {
-		String md5 = MD5Utils.md5("asdfsafd");
+		long now = System.currentTimeMillis();
+		
+		String md5 = MD5Utils.md5("1910259602" + "8b98f937554a180c9a6a" + now);
+		System.out.println("now:" + now);
+		System.out.println("md5:" + md5.toLowerCase());
 		boolean isOk = md5 != null && md5.length() > 30;
 		assertTrue("testBuild", isOk);
 	}
