c68c63310ef06d67dd6f02247fa7654f83814bf4
lijianqiang <newphper@gmail.com>
2019-02-18 11:17:23 +0800

    3.1.2

---
+++ src/main/java/com/bytehonor/protocol/core/server/result/EmptyResultVO.java
@@ -0,0 +1,21 @@
+package com.bytehonor.protocol.core.server.result;
+
+public class EmptyResultVO {
+	
+	private String result;
+	
+	public EmptyResultVO() {
+		this.result = "OK";
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	
+
+}
