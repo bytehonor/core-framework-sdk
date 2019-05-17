56d91c1a9b3fc583eb34b72705931fcd08bbf256
lijianqiang <newphper@gmail.com>
2019-05-17 12:27:27 +0800

    clear

--- src/main/java/com/bytehonor/sdk/protocol/common/CommonProtocolSdkApplication.java
+++ src/main/java/com/bytehonor/sdk/protocol/common/CommonProtocolSdkApplication.java
@@ -1,12 +1,7 @@
 package com.bytehonor.sdk.protocol.common;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CommonProtocolSdkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonProtocolSdkApplication.class, args);
 	}
 }
