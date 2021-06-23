ead46c31853beee442f7e08d1bc7fe89a781cb44
lijianqiang <343891954@qq.com>
2021-06-23 19:28:28 +0800

    4.1.10

---
+++ src/main/java/com/bytehonor/sdk/define/bytehonor/util/RedisHelper.java
@@ -0,0 +1,17 @@
+package com.bytehonor.sdk.define.bytehonor.util;
+
+import java.util.Objects;
+
+import com.bytehonor.sdk.define.bytehonor.constant.RedisConstants;
+
+public class RedisHelper {
+
+	public static String formatKeyName(String rawKeyName) {
+	    Objects.requireNonNull(rawKeyName, "rawKeyName");
+		if (rawKeyName.startsWith(RedisConstants.PROJECT_PREFIX) == false) {
+			String newKeyName = new StringBuilder(RedisConstants.PROJECT_PREFIX).append(rawKeyName).toString();
+			return newKeyName;
+		}
+		return rawKeyName;
+	}
+}
