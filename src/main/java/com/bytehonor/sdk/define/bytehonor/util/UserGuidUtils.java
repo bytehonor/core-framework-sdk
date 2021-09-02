cc5aa308e03f6300b3c562cfa668b43cc5695919
lijianqiang <newphper@126.com>
2021-09-02 14:35:51 +0800

    1.2.1

--- src/main/java/com/bytehonor/sdk/define/bytehonor/util/UserGuidUtils.java
+++ src/main/java/com/bytehonor/sdk/define/bytehonor/util/UserGuidUtils.java
@@ -1,12 +1,12 @@
 package com.bytehonor.sdk.define.bytehonor.util;
 
-public class UserUuidUtils {
+public class UserGuidUtils {
 
-    public static String uuidWeixin(String appid, String openid) {
+    public static String guidWeixin(String appid, String openid) {
         return MD5Utils.md5(new StringBuilder().append(appid).append(":").append(openid).toString());
     }
 
-    public static String uuidWeibo(Long uid) {
+    public static String guidWeibo(Long uid) {
         return MD5Utils.md5(new StringBuilder().append("weibo").append(":").append(uid).toString());
     }
 }
