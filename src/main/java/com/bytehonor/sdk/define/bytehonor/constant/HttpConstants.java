a4b848a56d8b80d01e1efd875d20c4afc19bbeda
lijianqiang <newphper@126.com>
2022-06-01 14:12:34 +0800

    1.3.1

--- src/main/java/com/bytehonor/sdk/define/bytehonor/constant/HttpConstants.java
+++ src/main/java/com/bytehonor/sdk/define/bytehonor/constant/HttpConstants.java
@@ -10,11 +10,11 @@ public class HttpConstants {
 
     public static final String LIMIT_KEY = "limit";
 
-    public static final Integer LIMIT_DEF = 20;
+    public static final int LIMIT_DEF = 20;
 
-    public static final Integer LIMIT_MAX = 200;
+    public static final int LIMIT_MAX = 200;
 
-    public static final Integer LIMIT_MAX_TOP = 1000;
+    public static final int LIMIT_MAX_TOP = 1000;
 
     public static final String OFFSET_KEY = "offset";
 
@@ -31,6 +31,8 @@ public class HttpConstants {
     public static final String SORT_DESC = "desc";
 
     public static final String PAGE_KEY = "page";
+
+    public static final int PAGE_DEF = 1;
 
     public static final String TOKEN_KEY = "token";
 
--- src/main/java/com/bytehonor/sdk/define/bytehonor/util/StringObject.java
+++ src/main/java/com/bytehonor/sdk/define/bytehonor/util/StringObject.java
@@ -6,6 +6,10 @@ package com.bytehonor.sdk.define.bytehonor.util;
  */
 public class StringObject {
 
+    private static final String EMPTY = "";
+
+    private static final char UNDERLINE = '_';
+
     public static boolean isEmpty(String str) {
         return (str == null || str.isEmpty());
     }
@@ -15,5 +19,53 @@ public class StringObject {
             return false;
         }
         return src1.equals(src2);
+    }
+
+    /**
+     * 字符串驼峰转下划线格式
+     *
+     * @param src 需要转换的字符串
+     * @return 转换好的字符串
+     */
+    public static String camelToUnderline(String src) {
+        if (StringObject.isEmpty(src)) {
+            return EMPTY;
+        }
+        int len = src.length();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = src.charAt(i);
+            if (Character.isUpperCase(c) && i > 0) {
+                sb.append(UNDERLINE);
+            }
+            sb.append(Character.toLowerCase(c));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 字符串下划线转驼峰格式
+     *
+     * @param src 需要转换的字符串
+     * @return 转换好的字符串
+     */
+    public static String underlineToCamel(String src) {
+        if (StringObject.isEmpty(src)) {
+            return EMPTY;
+        }
+        String temp = src.toLowerCase();
+        int len = temp.length();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = temp.charAt(i);
+            if (c == UNDERLINE) {
+                if (++i < len) {
+                    sb.append(Character.toUpperCase(temp.charAt(i)));
+                }
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
