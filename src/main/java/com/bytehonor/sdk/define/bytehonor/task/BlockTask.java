eb1d4b5424a3f7eefe70c4145080ef57fe42d19a
lijianqiang <newphper@126.com>
2022-06-21 15:27:30 +0800

    1.3.4

---
+++ src/main/java/com/bytehonor/sdk/define/bytehonor/task/BlockTask.java
@@ -0,0 +1,22 @@
+package com.bytehonor.sdk.define.bytehonor.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BlockTask extends SafeTask {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BlockTask.class);
+
+    public abstract void runInQueue();
+
+    @Override
+    public final void runInSafe() {
+        while (true) {
+            try {
+                runInQueue();
+            } catch (Exception e) {
+                LOG.error("runInQueue", e);
+            }
+        }
+    }
+}
