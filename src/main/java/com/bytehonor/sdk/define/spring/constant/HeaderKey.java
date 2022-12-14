43a9f7c752b551fbcdb7baa94fd536ef97786c97
lijianqiang <newphper@126.com>
2022-12-14 11:27:16 +0800

    0.3.6

--- src/main/java/com/bytehonor/sdk/define/spring/constant/HeaderKey.java
+++ src/main/java/com/bytehonor/sdk/define/spring/constant/HeaderKey.java
@@ -14,16 +14,4 @@ public class HeaderKey {
     public static final String REQUEST_UUID = "Request-Uuid";
 
     public static final String REQUEST_TERMINAL = "Request-Terminal";
-
-    @Deprecated
-    public static final String X_FROM_TERMINAL = "X-From-Terminal";
-
-    @Deprecated
-    public static final String X_ACCESS_TOKEN = "X-Access-Token";
-
-    @Deprecated
-    public static final String X_ACCESS_SIGN = "X-Access-Sign";
-
-    @Deprecated
-    public static final String X_ACCESS_TIME = "X-Access-Time";
 }
