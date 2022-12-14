a91c267380a4e478c2d6bb7ca0403ed0ac287dda
lijianqiang <newphper@126.com>
2022-12-14 11:33:15 +0800

    0.3.6

--- src/main/java/com/bytehonor/sdk/define/spring/constant/AuthKey.java
+++
@@ -1,8 +0,0 @@
-package com.bytehonor.sdk.define.spring.constant;
-
-public class AuthKey {
-
-    public static final String TOKEN_KEY = "token";
-
-    public static final String NO_AUTH = ".do";
-}
