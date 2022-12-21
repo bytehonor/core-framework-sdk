11b24294b75fc68c342cdec062dba64edf09072f
lijianqiang <newphper@126.com>
2022-12-21 10:50:12 +0800

    0.3.8

---
+++ src/main/java/com/bytehonor/sdk/define/spring/GreatScientist.java
@@ -0,0 +1,36 @@
+package com.bytehonor.sdk.define.spring;
+
+/**
+ * 20221221
+ * 
+ * @author lijianqiang
+ *
+ */
+public class GreatScientist {
+
+    /**
+     * 亨利·卡文迪许（Henry Cavendish，1731年10月10日—1810年2月24日）
+     */
+    public static final String CAVENDISH = "cavendish";
+
+    /**
+     * 迈克尔·法拉第 （Michael Faraday，1791年9月22日—1867年8月25日）
+     */
+    public static final String FARADAY = "faraday";
+
+    /**
+     * 沃纳·卡尔·海森堡（德文原名：Werner Karl Heisenberg，1901年12月5日—1976年2月1日）
+     */
+    public static final String HEISENBERG = "heisenberg";
+
+    /**
+     * 詹姆斯·克拉克·麦克斯韦（James Clerk Maxwell, 1831年6月13日—1879年11月5日）
+     */
+    public static final String MAXWELL = "maxwell";
+
+    /**
+     * 马克斯·卡尔·恩斯特·路德维希·普朗克（德语：Max Karl Ernst Ludwig Planck; 英文：Max Planck;
+     * 1858年4月23日—1947年10月4日）
+     */
+    public static final String PLANCK = "planck";
+}
