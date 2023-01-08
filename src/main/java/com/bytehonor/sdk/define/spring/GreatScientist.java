66eb8f76fa695b5b5816f3e8b5d5f65684a0e268
lijianqiang <343891954@qq.com>
2023-01-08 19:51:47 +0800

    0.3.9

--- src/main/java/com/bytehonor/sdk/define/spring/GreatScientist.java
+++ src/main/java/com/bytehonor/sdk/define/spring/GreatScientist.java
@@ -9,6 +9,46 @@ package com.bytehonor.sdk.define.spring;
 public class GreatScientist {
 
     /**
+     * 阿伏伽德罗（Amedeo Avogadro、1776年8月9日－1856年7月9日）
+     */
+    public static final String AVOGADRO = "Avogadro";
+
+    /**
+     * 安德烈·玛丽·安培（André-Marie Ampère，1775年1月20日 — 1836年6月10日）
+     */
+    public static final String AMPERE = "ampere";
+
+    /**
+     * 阿尔伯特·爱因斯坦（Albert Einstein）
+     */
+    public static final String EINSTEIN = "einstein";
+
+    /**
+     * 克里斯蒂安·约翰·多普勒（Christian Johann Doppler）
+     */
+    public static final String DOPPLER = "doppler";
+
+    /**
+     * 丹尼尔·伯努利（Daniel Bernoulli）
+     */
+    public static final String BERNOULLI = "bernoulli";
+
+    /**
+     * 海因里希·鲁道夫·赫兹（Heinrich Rudolf Hertz，1857年2月22日－1894年1月1日）
+     */
+    public static final String HERTZ = "hertz";
+
+    /**
+     * 亨德里克·安东·洛伦兹（1853年7月18日~1928年2月4日），英文名：Hendrik Antoon Lorentz
+     */
+    public static final String LORENTZ = "lorentz";
+
+    /**
+     * 约翰·卡尔·弗里德里希·高斯（Johann Carl Friedrich Gauss）
+     */
+    public static final String GAUSS = "gauss";
+
+    /**
      * 亨利·卡文迪许（Henry Cavendish，1731年10月10日—1810年2月24日）
      */
     public static final String CAVENDISH = "cavendish";
@@ -33,4 +73,19 @@ public class GreatScientist {
      * 1858年4月23日—1947年10月4日）
      */
     public static final String PLANCK = "planck";
+
+    /**
+     * 艾萨克·牛顿（Isaac Newton）
+     */
+    public static final String NEWTON = "newton";
+
+    /**
+     * 莱昂哈德·欧拉（Leonhard Euler）
+     */
+    public static final String EULER = "euler";
+
+    /**
+     * 埃尔温·薛定谔（Erwin Schrdinger）
+     */
+    public static final String SCHRDINGER = "schrdinger";
 }
